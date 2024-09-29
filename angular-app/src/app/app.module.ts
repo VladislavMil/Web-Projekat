22fb44e25bdec2e5ad443e2ce626887e1eb9b139
Lav <vladislav.lav.milinkovic@gmail.com>
2024-09-29 02:28:04 +0200

    mali dodaci

--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -3,15 +3,17 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponentComponent } from './components/login-component/login-component.component';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponentComponent
+    LoginComponent,
+    RegisterComponent,
   ],
   imports: [
     BrowserModule,
